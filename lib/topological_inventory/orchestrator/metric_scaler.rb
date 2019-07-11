3b0930d605db28fb411b88458b8dc3232fa6c221
Brandon Dunne <brandondunne@hotmail.com>
2019-07-11 14:57:41 -0400

    Values need to be strings for Openshift

--- lib/topological_inventory/orchestrator/metric_scaler.rb
+++ lib/topological_inventory/orchestrator/metric_scaler.rb
@@ -22,10 +22,10 @@ module TopologicalInventory
         object_manager.get_deployment_configs("metric_scaler_enabled=true").each do |dc|
           current_metric_name = dc.metadata.annotations["metric_scaler_current_metric_name"]       # i.e. "topological_inventory_api_puma_busy_threads"
           max_metric_name     = dc.metadata.annotations["metric_scaler_max_metric_name"]           # i.e. "topological_inventory_api_puma_max_threads"
-          max_replicas        = dc.metadata.annotations["metric_scaler_max_replicas"]&.to_i        # i.e. 5
-          min_replicas        = dc.metadata.annotations["metric_scaler_min_replicas"]&.to_i        # i.e. 1
-          target_usage_pct    = dc.metadata.annotations["metric_scaler_target_usage_pct"]&.to_i    # i.e. 50
-          scale_threshold_pct = dc.metadata.annotations["metric_scaler_scale_threshold_pct"]&.to_i # i.e. 20
+          max_replicas        = dc.metadata.annotations["metric_scaler_max_replicas"]&.to_i        # i.e. "5"
+          min_replicas        = dc.metadata.annotations["metric_scaler_min_replicas"]&.to_i        # i.e. "1"
+          target_usage_pct    = dc.metadata.annotations["metric_scaler_target_usage_pct"]&.to_i    # i.e. "50"
+          scale_threshold_pct = dc.metadata.annotations["metric_scaler_scale_threshold_pct"]&.to_i # i.e. "20"
 
           next unless current_metric_name && max_metric_name && max_replicas && min_replicas && target_usage_pct && scale_threshold_pct
           logger.info("Metrics scaling enabled for #{dc.metadata.name}")
